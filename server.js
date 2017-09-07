95ee0e617bc3a1d1d0057523585bb8ff7265ae76
Shimmyshanga <luca.junghans@gmail.com>
2017-09-06 18:20:28 -0700

    forcehttps

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const express     = require('express'),
       app         = express();
 // Run the app by serving the static files
 // in the dist directory
+const forceSSL = () => {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  };
+};
 app.use(compression());
 app.use(express.static(__dirname + '/dist'));
 const path = require('path');
+app.use(forceSSL());
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
